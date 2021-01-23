db1e20a2a316ad95abbc23aca7eb531937f834a2
cengiztonbul <cengiztonbul@outlook.com>
2021-01-24 00:27:57 +0300

    Sample AI

---
+++ Assets/SampleUsage/AntAI/Scripts/Transitions/WalkToIdle.cs
@@ -0,0 +1,12 @@
+﻿using FiniteStateMachine;
+using UnityEngine;
+
+public class WalkToIdle : Transition
+{
+	Vector3 target;
+
+	public override bool Condition()
+	{
+		return (transform.position - ((Walk)currentState).target).sqrMagnitude < 0.5f;
+	}
+}
