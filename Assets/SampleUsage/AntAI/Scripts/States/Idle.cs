e542ea0ad25fd4d3c92bb3461e730923c17b553d
cengiztonbul <cengiztonbul@outlook.com>
2021-01-23 23:52:39 +0300

    Sample AntAI

---
+++ Assets/SampleUsage/AntAI/Scripts/States/Idle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using FiniteStateMachine;
+
+namespace AntAI
+{
+	public class Idle : State
+	{
+		public override void OnStart()
+		{
+			GetComponent<Renderer>().material.color = new Color(0.3f, 0.6f, 0.6f);
+		}
+	}
+}
